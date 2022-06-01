2a28696291cfeb9fa1837d76686d35e95aecbb47
DevWallaceSv <devwallacesv@gmail.com>
2022-05-31 22:05:32 -0300

    Exercicio2

---
+++ Exercicio2.js
@@ -0,0 +1,19 @@
+//Faça um Programa que peça a idade e a altura de 5 pessoas,
+//armazene cada informação no seu respectivo array.
+//Imprima a média de idade e de altura.
+
+const prompt = require('prompt-sync')();
+console.clear();
+
+let idade;
+let altura;
+let mediaIdade;
+let mediaAltura;
+
+
+
+
+
+
+
+console.log();
